76f1aaaea56694fb2080000a84b2efe682b61047
Adam Brode <89703008+silfeid@users.noreply.github.com>
2025-06-27 12:16:02 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 27 11:50:33 2025
+
+@author: brodea
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='adamantium',
+    version='0.1',
+    packages=find_packages(),
+    author='Adam Brode',
+    author_email='brodeam@gmail.com',
+    description='Several simple custom functions for data analytics work.  Run adamantium.show_guidance() for an overview.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
